bf898162ddd854b1b8684b8c934f7249ab4d8438
Rahul Bohara <rahulbohara@Rahuls-MacBook-Pro.local>
2023-09-23 10:56:01 +1000

    Added postbuild hook in package.json file

--- start/routes.ts
+++ start/routes.ts
@@ -21,7 +21,17 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async () => {
-  return { hello: 'world' }
+  return { hello: 'world again' }
+})
+
+Route.get('/projects', async () => {
+  const products =  [
+      { id: 1, name: 'product 1' },
+      { id: 2, name: 'product 2' },
+      { id: 3, name: 'product 3' },
+      { id: 4, name: 'product 4' },
+    ]
+  return await products
 })
 
 Route.group(() => {

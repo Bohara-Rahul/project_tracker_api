00cd5b70e2d9430142844329fdcb4c730ff75673
Rahul Bohara <rahulbohara@Rahuls-MacBook-Pro.local>
2023-09-30 20:32:16 +1000

    Removed social authentication details from env.ts file

--- env.ts
+++ env.ts
@@ -28,14 +28,4 @@ export default Env.rules({
   PG_USER: Env.schema.string(),
   PG_PASSWORD: Env.schema.string.optional(),
   PG_DB_NAME: Env.schema.string(),
-
-  // google
-  GOOGLE_CLIENT_ID: Env.schema.string(),
-  GOOGLE_CLIENT_SECRET: Env.schema.string(),
-  GOOGLE_CALLBACK_URL: Env.schema.string(),
-
-  // github
-  GITHUB_CLIENT_ID: Env.schema.string(),
-  GITHUB_CLIENT_SECRET: Env.schema.string(),
-  GITHUB_CALLBACK_URL: Env.schema.string()
 })
